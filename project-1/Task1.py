fa4c7b3ddf4e7f15365f20c9092ec7a4facf6a09
Carlos Loureda <balanzeneto@gmail.com>
2019-07-06 00:47:21 +0200

    Project 1 - Task 1

--- project-1/Task1.py
+++ project-1/Task1.py
@@ -14,7 +14,18 @@ with open('calls.csv', 'r') as f:
 
 """
 TASK 1:
-How many different telephone numbers are there in the records? 
+How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+def getTelephonesInArray(phoneSet, arr):
+    for elem in arr:
+        phoneSet.add(elem[0])
+        phoneSet.add(elem[1])
+    return phoneSet
+
+phoneSet = set([])
+phoneSet = getTelephonesInArray(phoneSet, texts)
+phoneSet = getTelephonesInArray(phoneSet, calls)
+
+print("phone set length: ", len(phoneSet))
